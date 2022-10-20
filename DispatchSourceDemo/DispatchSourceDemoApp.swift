ff633f6a4b9101e85d3a5c4772486091cb0a07d3
Jeff Blagdon <jeffblagdon@gmail.com>
2022-10-20 09:19:04 -0300

    Initial Commit

---
+++ DispatchSourceDemo/DispatchSourceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DispatchSourceDemoApp.swift
+//  DispatchSourceDemo
+//
+//  Created by Jeffrey Blagdon (work) on 2022-10-20.
+//
+
+import SwiftUI
+
+@main
+struct DispatchSourceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
